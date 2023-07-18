42f9684dc63866cbb060ecd68dccb4951d64e2e3
deirdre <deirdre.loyall@gmail.com>
2023-07-17 22:26:27 -0400

    initial commit of C# experiments.

---
+++ basicExperiments/getInput.cs
@@ -0,0 +1,11 @@
+using System;
+
+class GetInput
+{
+  static void Main()
+  {
+    Console.WriteLine("Say something you coward!");
+    string response = Console.ReadLine();
+    Console.WriteLine("You said: '{0}'", response);
+  }
+}
