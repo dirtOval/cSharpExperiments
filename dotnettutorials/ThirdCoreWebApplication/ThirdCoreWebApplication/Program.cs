1de88af0138b2e4091569adbce8b245cda789e5c
deirdre <deirdre.loyall@gmail.com>
2023-08-03 10:10:06 -0400

    okay these tutorials keep making me make new empty projects so im just gonna start reusing them. this is the last one i swear!!

---
+++ dotnettutorials/ThirdCoreWebApplication/ThirdCoreWebApplication/Program.cs
@@ -0,0 +1,15 @@
+namespace ThirdCoreWebApplication
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var builder = WebApplication.CreateBuilder(args);
+            var app = builder.Build();
+
+            app.MapGet("/", () => "Hello World!");
+
+            app.Run();
+        }
+    }
+}
