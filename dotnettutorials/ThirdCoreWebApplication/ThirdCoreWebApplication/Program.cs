b96b189bfe17ab219ba15f0505550eda12bc0673
deirdre <deirdre.loyall@gmail.com>
2023-08-03 10:49:39 -0400

    i added a potato route to test application. rejoice!

--- dotnettutorials/ThirdCoreWebApplication/ThirdCoreWebApplication/Program.cs
+++ dotnettutorials/ThirdCoreWebApplication/ThirdCoreWebApplication/Program.cs
@@ -8,6 +8,7 @@ namespace ThirdCoreWebApplication
             var app = builder.Build();
 
             app.MapGet("/", () => "Hello World!");
+            app.MapGet("/potato", () => "i hate myself lol");
 
             app.Run();
         }
