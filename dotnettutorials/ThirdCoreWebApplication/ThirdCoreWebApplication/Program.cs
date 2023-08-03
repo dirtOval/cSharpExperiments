d1aab52e225b948796f929e61b7f59cde7a73822
deirdre <deirdre.loyall@gmail.com>
2023-08-03 13:26:21 -0400

    added POTATO_EXISTS environment variable, very important and useful

--- dotnettutorials/ThirdCoreWebApplication/ThirdCoreWebApplication/Program.cs
+++ dotnettutorials/ThirdCoreWebApplication/ThirdCoreWebApplication/Program.cs
@@ -7,7 +7,19 @@ namespace ThirdCoreWebApplication
             var builder = WebApplication.CreateBuilder(args);
             var app = builder.Build();
 
-            app.MapGet("/", () => "Hello World!");
+            if (app.Environment.IsDevelopment())
+            {
+                Console.WriteLine("im gay");
+            }
+
+            if (Environment.GetEnvironmentVariable("POTATO_EXISTS") == "PotatoExists")
+            {
+                Console.WriteLine("oh my god you guys potato exists oh fuck oh shit");
+                Console.WriteLine("fuckkkkkk");
+            }
+
+            app.MapGet("/", () => "Worker process name : " + 
+            System.Diagnostics.Process.GetCurrentProcess().ProcessName);
             app.MapGet("/potato", () => "i hate myself lol");
 
             app.Run();
