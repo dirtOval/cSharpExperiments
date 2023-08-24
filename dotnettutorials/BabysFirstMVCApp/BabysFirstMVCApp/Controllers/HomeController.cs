3d92442287a4fc974e51695789a55968a3c89f95
deirdre <deirdre.loyall@gmail.com>
2023-08-24 19:36:21 -0400

    made an MVC project! time 2 learn 8)

---
+++ dotnettutorials/BabysFirstMVCApp/BabysFirstMVCApp/Controllers/HomeController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Mvc;
+namespace BabysFirstMVCApp.Controllers
+{
+    public class HomeController : Controller
+    {
+        public string Index()
+        {
+            return "This is index action from MVC Controller";
+        }
+    }
+}
---
+++ dotnettutorials/BabysFirstMVCApp/BabysFirstMVCApp/Program.cs
@@ -0,0 +1,23 @@
+namespace BabysFirstMVCApp
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var builder = WebApplication.CreateBuilder(args);
+            builder.Services.AddMvc();
+
+            var app = builder.Build();
+
+            app.UseRouting();
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapDefaultControllerRoute();
+            });
+
+            //app.MapGet("/", () => "Hello World!");
+
+            app.Run();
+        }
+    }
+}
